51dcba1a3536bad9676ddd38e2c3899b8d06305e
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-06-04 23:03:47 +0800

    fixed the white top bg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,9 +4,11 @@ import MainNav from "../landing/mainnav";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <MainNav />
-      <div className="h-[calc(100vh-72px)] flex items-center justify-center bg-black no-scroll">
-        {children}
+      <div className="bg-black">
+        <MainNav />
+        <div className="h-[calc(100vh-72px)] flex items-center justify-center bg-black no-scroll">
+          {children}
+        </div>
       </div>
     </>
   );
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -6,17 +6,19 @@ const LandingPage = () => {
   return (
     <div>
       {/* Navbar */}
-      <MainNav />
-      {/* Landing Page Content */}
-      <div className="bg-black h-[calc(100vh-72px)] text-white bg-full flex flex-col justify-center items-center">
-        <Image
-          width={50}
-          height={50}
-          src="grimoire-logo.svg"
-          alt="Logo"
-          className="pb-16 h-auto w-auto mb-4"
-        />
-        <h1 className="text-4xl">Welcome to Our Website!</h1>
+      <div className="bg-black">
+        <MainNav />
+        {/* Landing Page Content */}
+        <div className="bg-black h-[calc(100vh-72px)] text-white bg-full flex flex-col justify-center items-center">
+          <Image
+            width={50}
+            height={50}
+            src="grimoire-logo.svg"
+            alt="Logo"
+            className="pb-16 h-auto w-auto mb-4"
+          />
+          <h1 className="text-4xl">Welcome to Our Website!</h1>
+        </div>
       </div>
     </div>
   );
