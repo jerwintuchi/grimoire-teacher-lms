35f66269f85cfb57bec7dced405fb4dd717f67f7
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-06-23 06:17:40 +0800

    fix: refactored the teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,12 +1,15 @@
-import { useUser } from "@clerk/nextjs";
+interface User {
+  publicMetadata: {
+    role?: string;
+  };
+}
 
-export const isTeacher = (userId?: string | null) => {
-  const user = useUser();
+export const isTeacher = (userId?: string | null, user?: User | null) => {
   if (userId === process.env.NEXT_PUBLIC_TEACHER_ID) {
-    //if an .env variable is set to a valid user id, then it's a teacher
+    // if an .env variable is set to a valid user id, then it's a teacher
     return true;
-  } else if (user?.user?.publicMetadata?.role !== "student" || "") {
-    //else if the publicmetadata is not set to student or empty
+  } else if (user?.publicMetadata?.role !== "student" || "") {
+    // else if the publicmetadata is not set to student or empty
     return true;
   } else {
     return false;
