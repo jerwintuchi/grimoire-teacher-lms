06053ef1fdae4a95a79d175fd41a64ac46c81330
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-06-24 03:22:39 +0800

    fix: removed unnecessary checking of roles within the dashboard

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -7,21 +7,22 @@ import { redirect } from "next/navigation";
 export default async function TeacherDashBoard() {
   const user = await currentUser();
 
-  if (checkRole("teacher") && user) {
-    // Redirect to root for teacher or publicMetadata.role === "teacher"
-    return redirect("/");
-  } else if (
+  // if (checkRole("teacher") && user) {
+  //   // Redirect to root for teacher or publicMetadata.role === "teacher"
+  //   return redirect("/");
+  // } else \
+  if (
     //if role is student or have no role then unauthorized
-    (checkRole("student") && user) ||
+    checkRole("student") ||
     (!checkRole("student") && !checkRole("teacher"))
   ) {
     //from checkRole("student") to role
     return redirect("/unauthorized");
   }
 
-  if (!user) {
-    return redirect("/unauthorized");
-  }
+  // if (!user) {
+  //   return redirect("/unauthorized");
+  // }
 
   return (
     <div>
