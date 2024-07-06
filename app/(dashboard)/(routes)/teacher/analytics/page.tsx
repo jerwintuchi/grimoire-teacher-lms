6636713827e678a754b547753a6ea242027fbfb3
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-07-07 05:05:44 +0800

    ftr: added transactionsCard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { getAnalytics } from "@/actions/get-analytics";
 import { DataCard } from "./_components/data-card";
 import { Chart } from "./_components/chart";
+import TransactionsCard from "./_components/transactions";
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
@@ -15,13 +16,12 @@ const AnalyticsPage = async () => {
 
   return (
     <div className="p-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
         <DataCard value={totalSales} shouldFormat={false} label="Total Sales" />
         <DataCard value={totalRevenue} label="Total Revenue" />
       </div>
-      <Chart
-      data={data}
-      />
+      <Chart data={data} />
+      <TransactionsCard />
     </div>
   );
 };
