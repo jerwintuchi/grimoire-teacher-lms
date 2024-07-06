54e7a9b2d208e298c6f0b8be72e659c80e87bdff
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-07-07 05:41:54 +0800

    removed unused routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,11 +2,9 @@
 
 import { UserButton, useAuth, useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { Button } from "./ui/button";
-import { LogOut } from "lucide-react";
-import Link from "next/link";
+
 import { GreetUser } from "./greetings/greetuser";
-import { SearchInput } from "./search-input";
+import { Badge } from "./ui/badge";
 
 const NavbarRoutes = () => {
   const { userId } = useAuth();
@@ -14,46 +12,21 @@ const NavbarRoutes = () => {
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
-  const isSearchPage = pathname === "/search";
 
   return (
     <>
-      {isSearchPage && (
-        <div className="hidden md:block text-[#b98ee4]">
-          <SearchInput />
-        </div>
-      )}
       <div className="flex gap-x-2 ml-auto">
-        {isTeacherPage || isCoursePage ? (
-          <div>
-            <GreetUser />
-            <Link href="/search">
-              <Button
-                size="sm"
-                className="text-[#b98ee4] bg-[#291839]
-                    hover:bg-[#853bce] hover:text-white border-red-900 size-auto">
-                <LogOut className="h-4 w-4 mr-2" />
-                Back
-              </Button>
-            </Link>
-          </div> //isTeacher is from .env and teacherUser is from sessionClaims
-        ) : (
-          (
-            <div>
-              <GreetUser />
-              <Link href="/teacher/courses">
-                <Button
-                  size="sm"
-                  variant="ghost"
-                  className="text-[#b98ee4] bg-[#291839]
-                    hover:bg-[#853bce] hover:text-white border-red-900 size-auto">
-                  Teacher Mode
-                </Button>
-              </Link>
-            </div>
-          ) || null
-        )}
-
+        {" "}
+        ( (
+        <div>
+          <GreetUser />
+          <Badge
+            className="text-[#b98ee4] bg-[#291839]
+                    hover:bg-[#853bce] hover:text-white border-[#853bce] px-8 size-auto">
+            Teacher
+          </Badge>
+        </div>
+        ) )
         <UserButton afterSignOutUrl="/sign-in" />
       </div>
     </>
