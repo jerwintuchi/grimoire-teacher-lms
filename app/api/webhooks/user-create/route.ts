83a090c2b35ea1ce7f9493170154ad78649d5784
jerwintuchi <jerwin.tuchi3@gmail.com>
2024-07-09 02:01:37 +0800

    fix: teacher creation webhook successfully creates the teacher metadata when manually created in clerk

---
+++ app/api/webhooks/user-create/route.ts
@@ -0,0 +1,105 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { headers } from "next/headers";
+import { IncomingHttpHeaders } from "http";
+import { NextResponse } from "next/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+
+const prisma = new PrismaClient();
+const webhookSecret = process.env.ADMIN_CRUD_WEBHOOK_SECRET || "";
+
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+
+  const wh = new Webhook(webhookSecret);
+
+  type EventType = "user.created" | "user.updated" | "user.deleted";
+
+  interface ClerkEvent {
+    data: {
+      id: string;
+      publicMetadata?: Record<string, any>;
+      privateMetadata?: Record<string, any>;
+    };
+    object: "event";
+    type: EventType;
+  }
+
+  let evt: ClerkEvent | null = null;
+
+  try {
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as ClerkEvent;
+  } catch (err) {
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
+
+  const eventType = evt.type;
+
+  const { id, publicMetadata, ...attributes } = evt.data;
+  const teacherrole = publicMetadata?.role || "teacher";
+
+  //LOGIC FOR FIRST TIME USER CREATION
+  if (eventType === "user.created") {
+    await clerkClient.users.updateUserMetadata(id, {
+      publicMetadata: {
+        role: teacherrole,
+      },
+    });
+
+    await prisma.user.create({
+      data: {
+        clerkId: id,
+        clerkAttributes: {
+          attributes,
+        },
+        role: teacherrole, // Add this line
+      },
+    });
+    return NextResponse.json(
+      { message: "User is created as teacher by default" },
+      { status: 200 }
+    );
+  }
+
+  if (eventType === "user.updated") {
+    const updatedUser = await clerkClient.users.getUser(evt.data.id);
+    await prisma.user.update({
+      where: { clerkId: evt.data.id },
+      data: {
+        clerkAttributes: {
+          attributes,
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: "User is now teacher" },
+      { status: 200 }
+    );
+  } else if (eventType === "user.deleted") {
+    await prisma.user.delete({
+      where: { clerkId: evt.data.id },
+    });
+
+    return NextResponse.json({ message: "User deleted" }, { status: 200 });
+  }
+
+  return NextResponse.json(
+    { message: "Unhandled event type" },
+    { status: 400 }
+  );
+}
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
